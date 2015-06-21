b33aaf22f54626f21d326823759165a51e19a668
Brendan McMahon <bernos@gmail.com>
2015-06-21 11:23:48 +1000

    Fixed bug where environment events were being logged twice

--- ebdeploy/bluegreenstrategy.go
+++ ebdeploy/bluegreenstrategy.go
@@ -109,7 +109,7 @@ func prepareTargetEnvironment(ctx *DeploymentContext, next Continue) error {
 	if environments, err := ebService.GetEnvironments(ctx.Configuration.ApplicationName); err == nil {
 
 		log.Printf("Found %d existing environments", len(environments))
-
+		requiresTerminate := false
 		activeCname := calculateCnamePrefix(ctx.Configuration.ApplicationName, ctx.Environment, true)
 		inactiveCname := calculateCnamePrefix(ctx.Configuration.ApplicationName, ctx.Environment, false)
 
@@ -125,19 +125,7 @@ func prepareTargetEnvironment(ctx *DeploymentContext, next Continue) error {
 				IsActive: false,
 			}
 
-			if err := ebService.TerminateEnvironment(*inactiveEnvironment.EnvironmentID); err != nil {
-				return err
-			}
-
-			done := make(chan struct{})
-			defer close(done)
-			ebService.LogEnvironmentEvents(ctx.Configuration.ApplicationName, ctx.TargetEnvironment.Name, done)
-
-			if err := ebService.WaitForEnvironment(ctx.Configuration.ApplicationName, ctx.TargetEnvironment.Name, func(e *elasticbeanstalk.EnvironmentDescription) bool {
-				return *e.Status == "Terminated"
-			}); err != nil {
-				return err
-			}
+			requiresTerminate = true
 		} else if activeEnvironment == nil && inactiveEnvironment == nil {
 			log.Println("Neither active nor inactive environments were found. Deploying directly to active environment")
 
@@ -166,6 +154,22 @@ func prepareTargetEnvironment(ctx *DeploymentContext, next Continue) error {
 		}
 
 		ctx.TargetEnvironment.Url = "http://" + ctx.TargetEnvironment.CNAME + ".elasticbeanstalk.com"
+
+		done := make(chan struct{})
+		defer close(done)
+		ebService.LogEnvironmentEvents(ctx.Configuration.ApplicationName, ctx.TargetEnvironment.Name, done)
+
+		if requiresTerminate {
+			if err := ebService.TerminateEnvironment(*inactiveEnvironment.EnvironmentID); err != nil {
+				return err
+			}
+
+			if err := ebService.WaitForEnvironment(ctx.Configuration.ApplicationName, ctx.TargetEnvironment.Name, func(e *elasticbeanstalk.EnvironmentDescription) bool {
+				return *e.Status == "Terminated"
+			}); err != nil {
+				return err
+			}
+		}
 
 		log.Printf("active environment: %v", activeEnvironment)
 		log.Printf("inactive environment: %v", inactiveEnvironment)
@@ -197,10 +201,6 @@ func deployApplicationVersion(ctx *DeploymentContext, next Continue) error {
 	if resp, err := client.CreateEnvironment(params); err != nil {
 		return err
 	} else {
-
-		done := make(chan struct{})
-		defer close(done)
-		ebService.LogEnvironmentEvents(ctx.Configuration.ApplicationName, ctx.TargetEnvironment.Name, done)
 
 		log.Printf("Deployed: %v", resp)
 		log.Printf("Waiting for environment health to go green")
